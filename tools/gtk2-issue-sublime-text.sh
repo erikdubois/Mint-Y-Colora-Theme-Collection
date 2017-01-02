1429ec2aa3fb9bde79dfd7f2cf76ca976e66f675
Erik Dubois <erik.dubois@gmail.com>
2017-01-02 20:50:52 +0100

    Comment :  on 20:50:52 02-01-2017

---
+++ tools/gtk2-issue-sublime-text.sh
@@ -0,0 +1,190 @@
+#!/bin/bash -x
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+name=Arc
+
+# fix for gtk2 sublime text select menu still green
+
+cd Mint-Y-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Dark-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Darker-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+
+name=Crimson
+
+# fix for gtk2 sublime text select menu still green
+
+cd Mint-Y-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Dark-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Darker-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+
+name=Evopop
+
+# fix for gtk2 sublime text select menu still green
+
+cd Mint-Y-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Dark-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Darker-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+name=Froly
+
+# fix for gtk2 sublime text select menu still green
+
+cd Mint-Y-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Dark-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Darker-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+
+name=Majestic
+
+# fix for gtk2 sublime text select menu still green
+
+cd Mint-Y-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Dark-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Darker-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+
+name=Orange
+
+# fix for gtk2 sublime text select menu still green
+
+cd Mint-Y-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Dark-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Darker-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+
+name=Polo
+
+# fix for gtk2 sublime text select menu still green
+
+cd Mint-Y-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Dark-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Darker-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+
+name=Smoke
+
+# fix for gtk2 sublime text select menu still green
+
+cd Mint-Y-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Dark-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Darker-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+
+name=Watusi
+
+# fix for gtk2 sublime text select menu still green
+
+cd Mint-Y-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Dark-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Dark-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
+
+cd Mint-Y-Darker-$name
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button-dark.png
+cp gtk-2.0/assets/menubar_button.png ~/.themes/Mint-Y-Darker-$name/gtk-2.0/menubar-toolbar/menubar_button.png
+cd ..
